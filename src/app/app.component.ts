2b6977abfbbee0a85a6b73c8abbf1c3169861880
Dave <daveharmswebdev@gmail.com>
2017-08-21 16:11:29 -0500

    added button and login with google, part of app.component will move to navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './login/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Chat Demo';
+  user = null;
+
+  constructor(private auth: AuthService) {}
+
+  ngOnInit() {
+    this.auth.getAuthState().subscribe(user => {
+      this.user = user;
+      console.log(user);
+    });
+  }
+
+  loginWithGoogle() {
+    this.auth.loginWithGoogle().then(data => {
+      console.log(data);
+    });
+  }
+
 }
